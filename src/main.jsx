53007039e174c1416339c8e80cc6ec4a7a71d94a
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-10-16 00:46:09 +0530

    Refactor search bar component, add link to search page, and update dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import "./index.css";
+import { AuthContextProvider } from "./contexts/AuthContext.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </StrictMode>
+);
